c18376bbe03fef6ce5a94fd1810d447d8e984082
harounaKane <harouna.kane@hotmail.fr>
2023-05-09 09:55:50 +0200

    mise en place projet

---
+++ src/main/java/com/ilci/magasin/MagasinCorrigeApplication.java
@@ -0,0 +1,13 @@
+package com.ilci.magasin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagasinCorrigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagasinCorrigeApplication.class, args);
+	}
+
+}
